16257b968251feb15bf3ff6d8a2e3c2a6a222c10
AntonVoronezh <pstut@mail.ru>
2022-07-06 15:54:14 +0300

    add form

--- notes/app.js
+++ notes/app.js
@@ -2,7 +2,16 @@ const App = {
   data() {
     return {
       title: "vue title",
+      input: {
+        value: "",
+        placeholder: "type or not",
+      },
     };
+  },
+  methods: {
+    onSubmit() {
+        console.log(this.input)
+    }
   },
 };
 
