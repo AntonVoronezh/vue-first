9fc728fea756e5e54723c881dcd82edf6c0745f5
AntonVoronezh <pstut@mail.ru>
2022-07-07 15:07:35 +0300

    watch

--- notes/app.js
+++ notes/app.js
@@ -6,14 +6,33 @@ const App = {
         value: "",
         placeholder: "type or not",
       },
-      notes: []
+      notes: ["aaaaa", "bbbbb", "ccccc"],
     };
   },
+  mounted() {
+    this.getNotes();
+  },
+  watch: {
+    notes: {
+      handler(updateList) {
+        localStorage.setItem("notes", JSON.stringify(updateList));
+      },
+    },
+  },
   methods: {
+    getNotes() {
+      const notes = localStorage.getItem("notes");
+
+      if (notes) {
+        this.notes = JSON.parse(notes);
+      }
+    },
     onSubmit() {
-      this.notes.push(this.input.value)
-        console.log(this.input)
-    }
+      this.notes.push(this.input.value);
+    },
+    remove(index) {
+      this.notes.splice(index, 1);
+    },
   },
 };
 
