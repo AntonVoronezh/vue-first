e5d5a4a031c6d3896daab22d0400a54254667d8e
AntonVoronezh <pstut@mail.ru>
2022-07-08 13:11:05 +0300

    add style

--- ddd/src/App.vue
+++ ddd/src/App.vue
@@ -1,7 +1,22 @@
-<template>gggggg</template>
+<template>
+  <div class="wrapper">
+    <TheHeader />
+    <div class="wrapper-content">
+      <div class="container">
+        gggggggggggg
+      </div>
+      </div>
+    <TheFooter />
+  </div>
+</template>
 
 <script>
-export default {};
+import TheHeader from "@/components/TheHeader";
+import TheFooter from "@/components/TheFooter";
+
+export default {
+  components: { TheFooter, TheHeader },
+};
 </script>
 
 <style lang="scss"></style>
