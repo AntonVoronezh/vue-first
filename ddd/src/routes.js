63dbefd1e4bfe31b5043ac51c0abd71db2d19804
AntonVoronezh <pstut@mail.ru>
2022-07-11 15:29:11 +0300

    404

--- ddd/src/routes.js
+++ ddd/src/routes.js
@@ -7,17 +7,25 @@ const routerHistory = createWebHashHistory();
 
 import HomePage from "./pages/homePage";
 import AboutPage from "./pages/aboutPage";
+import NotFoundPage from "./pages/notFound";
 
 const routes = createRouter({
   history: routerHistory,
   routes: [
     {
       path: "/",
+      name: 'home',
       component: HomePage,
     },
     {
       path: "/about",
+      name: 'about',
       component: AboutPage,
+    },
+    {
+      path: "/:CatchAll(.*)",
+      name: '404',
+      component: NotFoundPage,
     },
   ],
 });
